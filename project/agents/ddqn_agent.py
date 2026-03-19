d6da5523281d065a1ca8b0b3d475e13dafe3ae91
D21W12 <daanplayerspecial@gmail.com>
2026-03-19 14:15:30 +0100

    add double dqn

---
+++ project/agents/ddqn_agent.py
@@ -0,0 +1,18 @@
+import torch
+
+from .dqn_agent import DQNAgent
+
+
+class DDQNAgent(DQNAgent):
+
+    def _loss(self, s, a, r, s_prime, t) -> torch.Tensor:
+        # We detach the computational graph of the target, because
+        # we do not need to compute gradients for the target network.
+        with torch.no_grad():
+            a_prime = self._dqn(s_prime).argmax(dim=1, keepdims=True)
+            q_s_prime = self._target_dqn(s_prime).gather(dim=1, index=a_prime).squeeze(1)
+            targets = r + (self._discount * q_s_prime) * (~t)
+
+        q_values = self._dqn(s)
+        q_values = q_values.gather(dim=1, index=a.unsqueeze(1)).squeeze(1)
+        return self._criterion(targets, q_values)
