6c3fe5726253d2d4f72af46314100aabadbe885a
D21W12 <daanplayerspecial@gmail.com>
2026-05-09 17:50:26 +0200

    add eval settings

--- project/eval/config.py
+++ project/eval/config.py
@@ -4,12 +4,12 @@ from dataclasses import dataclass, field
 @dataclass
 class DiffEvalConfig:
     network: str = "edm2"
-    resolution: int = 16
-    in_channels: int = 3
-    start_channels: int = 64
+    resolution: int = 88
+    in_channels: int = 4
+    start_channels: int = 128
     num_res_blocks: int = 2
-    channel_multipliers: list[int] | tuple[int] = field(default_factory=lambda: [1, 2])
-    attention_resolutions: list[int] | tuple[int] | set[int] = field(default_factory=lambda: [8])
+    channel_multipliers: list[int] | tuple[int] = field(default_factory=lambda: [2, 2, 2])
+    attention_resolutions: list[int] | tuple[int] | set[int] = field(default_factory=lambda: [11])
     dropout: int = 0.13
 
 
