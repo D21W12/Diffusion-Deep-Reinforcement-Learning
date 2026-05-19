6ebc34cf075c2efe6c1bb0a1fc17ce3171039cc2
D21W12 <daan.wichmann@ru.nl>
2026-05-19 12:17:21 +0200

    remove redundant files

--- project/train/config.py
+++ project/train/config.py
@@ -25,11 +25,11 @@ class DDQNTrainingTrainingConfig(TrainingConfig):
     device: str
     checkpoint_path: str
     memory_checkpoint_path: str
-    lr: float = 1e-4
+    lr: float = 2.5e-5
     batch_size: int = 32
     discount: int = 0.99
     replay_size: int = 1_000_000
-    environment: str = "ALE/Breakout-v5"
+    environment: str = "ALE/SpaceInvaders-v5"
     target_update_frequency: int = 10_000
     final_exploration_frame: int = 1_000_000
     replay_start_size: int = 50_000
@@ -40,12 +40,12 @@ class DiffTrainingTrainingConfig(TrainingConfig):
     network: str = 'edm2'
     cap: int = 100_000
     batch_size: int = 32
-    lr: float = 2e-3
+    lr: float = 1e-3
     img_resolution: int = 88
     img_channels: int = 4
-    model_channels: int = 64
-    num_blocks: int = 2
-    channel_mult: list[int] | tuple[int] = field(default_factory=lambda: [1, 2,])
-    attn_resolutions: list[int] | tuple[int] | set[int] = field(default_factory=lambda: [22,])
+    model_channels: int = 128
+    num_blocks: int = 3
+    channel_mult: list[int] | tuple[int] = field(default_factory=lambda: [2, 2, 2])
+    attn_resolutions: list[int] | tuple[int] | set[int] = field(default_factory=lambda: [22])
     dropout: int = 0.13
-    mixed_precision: bool = True
+    mixed_precision: bool = False
