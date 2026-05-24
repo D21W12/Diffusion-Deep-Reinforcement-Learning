8c0e968fdbd42035f4baba1a13e0aea9d69c8d5d
D21W12 <daanplayerspecial@gmail.com>
2026-05-24 16:49:02 +0200

    fix denoisers

--- project/util/denoisers.py
+++ project/util/denoisers.py
@@ -14,8 +14,10 @@ class MedianFilter(NonLinearFilter):
     ):
         self._kernel_size = kernel_size
 
-    def denoise(self, x) -> torch.Tensor:
-        return ndimage.median_filter(x, self._kernel_size)
+    def denoise(self,  x: torch.Tensor) -> torch.Tensor:
+        x = x.clone()
+        y = ndimage.median_filter(x, self._kernel_size, output=x)
+        return torch.from_numpy(y)
 
 
 class GaussianFilter(NonLinearFilter):
@@ -26,5 +28,7 @@ class GaussianFilter(NonLinearFilter):
     ):
         self._sigma = sigma
 
-    def denoise(self, x) -> torch.Tensor:
-        return ndimage.gaussian_filter(x, self._sigma)
+    def denoise(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.clone()
+        y = ndimage.gaussian_filter(x, self._sigma)
+        return torch.from_numpy(y)
